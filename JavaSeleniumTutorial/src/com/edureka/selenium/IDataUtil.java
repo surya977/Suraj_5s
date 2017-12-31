d04f2b17d7b030483deca2cf74a74d0d0eb2eb92
SURYA PRAKASH SINGH <surya977gla@gmail.com>
2017-12-31 08:04:55 +0430

    bbb

---
+++ JavaSeleniumTutorial/src/com/edureka/selenium/IDataUtil.java
@@ -0,0 +1,14 @@
+package com.edureka.selenium;
+
+import java.util.List;
+import java.util.Map;
+
+public interface IDataUtil {
+	
+	public  List<Employee> getPersonData();
+	
+	public  Map<Integer,Person> getPersonMap();
+	
+	
+
+}
